1f6a95b47b9eaa126de349f149ab31b8825e64c8
Pedro Yan <134109214+pedroyanalmeida@users.noreply.github.com>
2024-06-21 16:54:45 -0300

    ajustes

--- assets/script.js
+++ assets/script.js
@@ -41,47 +41,47 @@ const incrementScore = () => {
 }
 
 const moveSnake = () => {
-    if (!direction) return; // Retorna se a direção não estiver definida
+    if (!direction) return; 
     
-    // Cria uma cópia da cabeça da cobra
+   
     const head = { x: snake[snake.length - 1].x, y: snake[snake.length - 1].y };
 
-    // Move a cabeça da cobra com base na direção atual
+   
     switch (direction.toLowerCase()) {
         case 'arrowright':
         case 'd':
             head.x += scale;
             if (head.x >= canvas.width) {
-                head.x = 0; // Reaparece do lado esquerdo
+                head.x = 0; 
             }
             break;
         case 'arrowleft':
         case 'a':
             head.x -= scale;
             if (head.x < 0) {
-                head.x = canvas.width - scale; // Reaparece do lado direito
+                head.x = canvas.width - scale;
             }
             break;
         case 'arrowup':
         case 'w':
             head.y -= scale;
             if (head.y < 0) {
-                head.y = canvas.height - scale; // Reaparece na parte inferior
+                head.y = canvas.height - scale; 
             }
             break;
         case 'arrowdown':
         case 's':
             head.y += scale;
             if (head.y >= canvas.height) {
-                head.y = 0; // Reaparece na parte superior
+                head.y = 0; 
             }
             break;
     }
 
-    // Verifica se a cobra tocou no próprio corpo
+    
     for (let i = 0; i < snake.length - 1; i++) {
         if (head.x === snake[i].x && head.y === snake[i].y) {
-            // Mostra tela de Game Over
+            
             clearInterval(loopId);
             document.getElementById('gameOverScreen').style.display = 'block';
             result.innerText = value.innerText;
@@ -91,49 +91,49 @@ const moveSnake = () => {
         }
     }
 
-    // Verifica se a cobra comeu a fruta
+    
     if (head.x === fruit.x && head.y === fruit.y) {
-        // Gera nova posição para a fruta
+        
         randomPosition();
 
-        // Adiciona novo segmento à cobra (não remove a cauda)
+        
         snake.push({ ...head });
         incrementScore();
     } else {
-        // Remove a cauda antiga (último segmento da cobra)
+       
         snake.shift();
         
-        // Adiciona a nova cabeça à cobra
+        
         snake.push(head);
     }
 };
 
 const randomPosition = () => {
-    // Gera posições aleatórias para x e y dentro dos limites do canvas
+    
     let newX = Math.floor(Math.random() * columns) * scale;
     let newY = Math.floor(Math.random() * rows) * scale;
 
-    // Verifica se a nova posição da fruta não está sobre a cobra
+    
     for (let segment of snake) {
         if (newX === segment.x && newY === segment.y) {
-            // Se estiver, gera novas coordenadas
+           
             randomPosition();
             return;
         }
     }
 
-    // Atualiza as coordenadas da fruta
+    
     fruit.x = newX;
     fruit.y = newY;
 };
 
 const drawGrid = () => {
-    ctx.save(); // Salva o estado atual do contexto
+    ctx.save(); 
     
     ctx.lineWidth = 1;
     ctx.strokeStyle = "white";
     
-    // Define a transparência global para 30% (0.3)
+  
     ctx.globalAlpha = 0.3;
 
     for (let i = 20; i < canvas.width; i += 20) {
@@ -148,9 +148,9 @@ const drawGrid = () => {
         ctx.stroke();
     }
 
-    ctx.restore(); // Restaura o estado anterior do contexto
+    ctx.restore(); 
 
-    // Desenhar a cobrinha (exemplo)
+   
     ctx.fillStyle = "gray";
     snake.forEach((position, index) => {
         if (index === snake.length - 1) {
@@ -173,7 +173,6 @@ const gameLoop = () => {
     }, 250);
 }
 
-// Inicia o jogo pela primeira vez
 gameLoop();
 document.addEventListener("keydown", (event) => {
     const key = event.key.toLowerCase();
@@ -208,24 +207,24 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
-// Configurações para o joystick
+
 document.addEventListener("DOMContentLoaded", function() {
     const aviso = document.getElementById("aviso");
     const startButton = document.getElementById("startButton");
     const joystickContainer = document.getElementById("joystickContainer");
     const joystickBase = document.getElementById("joystickBase");
 
-    let joystick; // Variável para armazenar a instância do joystick
+    let joystick; 
 
     const joystickOptions = {
-        zone: joystickBase, // Utiliza o joystickBase como zona interativa
+        zone: joystickBase, 
         mode: 'static',
         position: { left: '50%', top: '50%' },
         color: 'rgba(255,255,255,0.5)',
         size: 150
     };
 
-    // Função para criar o joystick
+
     function createJoystick() {
         joystick = nipplejs.create(joystickOptions);
 
@@ -251,33 +250,37 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
 
-        // Função para verificar se está movendo horizontalmente
+       
         const isMovingHorizontally = () => {
             return direction === 'arrowright' || direction === 'arrowleft';
         };
 
-        // Função para verificar se está movendo verticalmente
+     
         const isMovingVertically = () => {
             return direction === 'arrowup' || direction === 'arrowdown';
         };
     }
 
-    // Evento de clique para iniciar o jogo
     startButton.addEventListener("click", function() {
-        // Mostrar o joystickContainer ao clicar no botão Start
+       
         aviso.style.display = "none";
-        joystickContainer.style.display = "block";
-
-        // Criar o joystick
-        createJoystick();
-
-        // Iniciar o jogo
+    
+      
+        if (window.innerWidth <= 798) {
+           
+            joystickContainer.style.display = "block";
+            
+       
+            createJoystick();
+        }
+    
+      
         gameLoop();
     });
 
-    // Evento de clique para o botão Recomeçar
+ 
     document.getElementById('restartButton').addEventListener('click', () => {
-        // Reiniciar as variáveis do jogo
+      
         snake = [
             { x: 200, y: 200 },
             { x: 220, y: 200 }
@@ -285,13 +288,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
         direction = null;
 
-        // Ocultar o botão de Recomeçar e o Game Over
+       
         document.getElementById('gameOverScreen').style.display = 'none';
 
-        // Limpar o intervalo anterior, se existir
+      
         clearInterval(loopId);
 
-        // Reiniciar o loop do jogo
+       
         gameLoop();
     });
 });
